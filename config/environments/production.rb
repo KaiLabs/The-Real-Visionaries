af7534ca5349f424f4dcb47a8144a9b223bad78f
shrlyhe <she@wesleyan.edu>
2017-05-09 20:31:33 -0400

    add mailer stuff to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,4 +93,24 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+
+
+  # Mailer stuff
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'https://boiling-dawn-28173.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "gmail.com",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: "she33213",
+    password: "jack8red"
+  }
+
+
 end
