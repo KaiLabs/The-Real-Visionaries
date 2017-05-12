9073bece568c01b15431e5b62b7f2f67a52d2486
shrlyhe <she@wesleyan.edu>
2017-05-12 14:33:35 -0400

    change link

--- db/migrate/20170510001509_add_columns_to_submissions.rb
+++ db/migrate/20170510001509_add_columns_to_submissions.rb
@@ -4,11 +4,7 @@ class AddColumnsToSubmissions < ActiveRecord::Migration[5.0]
     add_column :submissions, :season, :string
     add_column :submissions, :year, :date
     add_column :submissions, :compensation, :string
-<<<<<<< HEAD:db/migrate/20170510001509_add_columns_to_submissions.rb
     add_column :submissions, :country, :string
     add_column :submissions, :state, :string
-=======
-    add_column :submissions, :country, :country
->>>>>>> c4c5bab83550b18a22f77fcb87544a17cc6901b9:db/migrate/20170330220610_add_columns_to_submissions.rb
   end
 end
